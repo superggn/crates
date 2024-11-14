dbba251c246951e514ec09b5ec5709587a5f6481
superggn <939401399@qq.com>
2024-11-14 18:31:45 +0800

    temp

--- thumbor/src/main.rs
+++ thumbor/src/main.rs
@@ -1,2 +1,120 @@
+use anyhow::Result;
+use axum::{
+    extract::{Extension, Path},
+    handler::get,
+    http::{HeaderMap, HeaderValue, StatusCode},
+    AddExtensionLayer, Router,
+};
+use bytes::Bytes;
+use image::ImageOutputFormat;
+use lru::LruCache;
+use percent_encoding::{percent_decode_str, percent_encode, NON_ALPHANUMERIC};
+use serde::Deserialize;
+use std::{
+    collections::hash_map::DefaultHasher,
+    convert::TryInto,
+    hash::{Hash, Hasher},
+    sync::Arc,
+};
+use tokio::sync::Mutex;
+use tower::ServiceBuilder;
+use tracing::{info, instrument};
+
+mod engine;
 mod pb;
-fn main() {}
+
+use engine::*;
+use pb::*;
+
+#[derive(Deserialize)]
+struct Params {
+    spec: String,
+    url: String,
+}
+
+type Cache = Arc<Mutex<LruCache<u64, Bytes>>>;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+    let cache: Cache = Arc::new(Mutex::new(LruCache::new(1024)));
+    let app = Router::new()
+        .route("/image/:spec/:url", get(generate))
+        .layer(
+            ServiceBuilder::new()
+                .layer(AddExtensionLayer::new(cache))
+                .into_inner(),
+        );
+    let addr = "127.0.0.1:3000".parse().unwrap();
+    // 注意，这里打印出来的 test url 确实是 url_encoded 的版本，
+    //  但是不要直接从 iterm 里点开链接， 而是要复制到浏览器再打开
+    //  否则还是会炸， 妈的... iterm 的解析有问题...
+    print_test_url("https://images.pexels.com/photos/1562477/pexels-photo-1562477.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260");
+    info!("Listening on {addr}",);
+    println!("Listening on {addr}");
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn generate(
+    Path(Params { spec, url }): Path<Params>,
+    Extension(cache): Extension<Cache>,
+) -> Result<(HeaderMap, Vec<u8>), StatusCode> {
+    let spec: ImageSpec = spec
+        .as_str()
+        .try_into()
+        .map_err(|_| StatusCode::BAD_REQUEST)?;
+    let url: &str = &percent_decode_str(&url).decode_utf8_lossy();
+    let data = retrieve_image(&url, cache)
+        .await
+        .map_err(|_| StatusCode::BAD_REQUEST)?;
+    // todo 处理图片
+    let mut engine: Photon = data
+        .try_into()
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+    engine.apply(&spec.specs);
+
+    let img = engine.generate(ImageOutputFormat::Jpeg(85));
+    info!("Finished processing: image size {}", img.len());
+
+    let mut headers = HeaderMap::new();
+    let val = HeaderValue::from_static("image/jpeg");
+    headers.insert("content-type", val);
+
+    Ok((headers, img))
+}
+
+#[instrument(level = "info", skip(cache))]
+async fn retrieve_image(url: &str, cache: Cache) -> Result<Bytes> {
+    let mut hasher = DefaultHasher::new();
+    url.hash(&mut hasher);
+    let key = hasher.finish();
+    let g = &mut cache.lock().await;
+    let data = match g.get(&key) {
+        Some(v) => {
+            info!("Match cache {}", key);
+            v.to_owned()
+        }
+        None => {
+            info!("Retrieve url");
+            let resp = reqwest::get(url).await?;
+            let data = resp.bytes().await?;
+            g.put(key, data.clone());
+            data
+        }
+    };
+    Ok(data)
+}
+
+fn print_test_url(url: &str) {
+    use std::borrow::Borrow;
+    let spec1 = Spec::new_resize(500, 800, resize::SampleFilter::CatmullRom);
+    let spec2 = Spec::new_watermark(20, 20);
+    let spec3 = Spec::new_filter(filter::Filter::Marine);
+    let image_spec = ImageSpec::new(vec![spec1, spec2, spec3]);
+    let s: String = image_spec.borrow().into();
+    let test_image = percent_encode(url.as_bytes(), NON_ALPHANUMERIC).to_string();
+    println!("test url: http://localhost:3000/image/{s}/{test_image}");
+}
