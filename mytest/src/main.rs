93697b40ebb992653e62d522f2e7870100e8e7ee
superggn <939401399@qq.com>
2025-01-15 20:14:25 +0800

    temp

--- mytest/src/main.rs
+++ mytest/src/main.rs
@@ -1,28 +1,66 @@
-use futures::stream::{self, StreamExt};
-// use std::collections::Iterator;
-use tokio;
+use futures::prelude::*;
+use pin_project::pin_project;
+use std::{
+    pin::Pin,
+    task::{Context, Poll},
+};
+use tokio::{
+    fs,
+    io::{AsyncBufReadExt, AsyncRead, BufReader, Lines},
+};
 
-#[tokio::main]
+// extern crate proc_macro;
 
-async fn main() {
-    let my_stream = stream::iter(vec![1, 2, 3, 4]);
-    let job = my_stream.for_each(|item| async move {
-        println!("Got item: {}", item);
-    });
-    println!("haha");
-    std::thread::sleep(std::time::Duration::from_secs(1));
-    // tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
-    println!("sleep over");
-    job.await;
+// use proc_macro;
+
+/// LineStream 内部使用 tokio::io::Lines
+#[pin_project]
+#[derive(Debug)]
+struct LineStream<R> {
+    #[pin]
+    lines: Lines<BufReader<R>>,
+    field2: i32,
+    #[pin]
+    field3: i32,
 }
 
-struct AA {}
-
-use std::iter::Iterator;
-
-impl Iterator for AA {
-    type Item = usize;
-    fn next(&mut self) -> Option<usize> {
-        Some(1)
+impl<R: AsyncRead> LineStream<R> {
+    /// 从 BufReader 创建一个 LineStream
+    pub fn new(reader: BufReader<R>) -> Self {
+        Self {
+            lines: reader.lines(),
+            field2: 2,
+            field3: 3,
+        }
     }
+}
+
+/// 为 LineStream 实现 Stream trait
+impl<R: AsyncRead> Stream for LineStream<R> {
+    type Item = std::io::Result<String>;
+
+    fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        println!("try this: {:?}", self.field2);
+        println!("try this3 self: {:?}", self.field3);
+        let project = self.project();
+        println!("try this2 project: {:?}", project.field2);
+        println!("try this3 project: {:?}", project.field3);
+        project.lines.poll_next_line(cx).map(Result::transpose)
+        // self.project()
+        //     .lines
+        //     .poll_next_line(cx)
+        //     .map(Result::transpose)
+    }
+}
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let numbers = vec![1, 2, 3];
+    let file = fs::File::open("Cargo.toml").await?;
+    let reader = BufReader::new(file);
+    let mut st = LineStream::new(reader);
+    while let Some(Ok(line)) = st.next().await {
+        println!("Got: {}", line);
+    }
+    Ok(())
 }
