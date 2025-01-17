001424264739b54717ce95f2702578713bc1be35
superggn <939401399@qq.com>
2025-01-17 20:44:28 +0800

    temp

--- mytest/src/main.rs
+++ mytest/src/main.rs
@@ -9,19 +9,11 @@ use tokio::{
     io::{AsyncBufReadExt, AsyncRead, BufReader, Lines},
 };
 
-// extern crate proc_macro;
-
-// use proc_macro;
-
 /// LineStream 内部使用 tokio::io::Lines
 #[pin_project]
-#[derive(Debug)]
 struct LineStream<R> {
     #[pin]
     lines: Lines<BufReader<R>>,
-    field2: i32,
-    #[pin]
-    field3: i32,
 }
 
 impl<R: AsyncRead> LineStream<R> {
@@ -29,33 +21,26 @@ impl<R: AsyncRead> LineStream<R> {
     pub fn new(reader: BufReader<R>) -> Self {
         Self {
             lines: reader.lines(),
-            field2: 2,
-            field3: 3,
         }
     }
 }
+
+// use std::result::Result;
 
 /// 为 LineStream 实现 Stream trait
 impl<R: AsyncRead> Stream for LineStream<R> {
     type Item = std::io::Result<String>;
 
     fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
-        println!("try this: {:?}", self.field2);
-        println!("try this3 self: {:?}", self.field3);
-        let project = self.project();
-        println!("try this2 project: {:?}", project.field2);
-        println!("try this3 project: {:?}", project.field3);
-        project.lines.poll_next_line(cx).map(Result::transpose)
-        // self.project()
-        //     .lines
-        //     .poll_next_line(cx)
-        //     .map(Result::transpose)
+        self.project()
+            .lines
+            .poll_next_line(cx)
+            .map(Result::transpose)
     }
 }
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let numbers = vec![1, 2, 3];
     let file = fs::File::open("Cargo.toml").await?;
     let reader = BufReader::new(file);
     let mut st = LineStream::new(reader);
