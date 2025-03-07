34e0c36947014c5c2300c75ec26e9b5d30086a25
superggn <939401399@qq.com>
2025-03-07 19:03:16 +0800

    temp

--- mytest/src/main.rs
+++ mytest/src/main.rs
@@ -1,34 +1,12 @@
-use bytes::{Buf, BufMut, Bytes, BytesMut};
-
 fn main() {
-    let mut buf = Bytes::from("hello world");
-    println!("{:?}", buf); // "hello world"
-    println!("{:?}", buf.len());
-    println!("====================");
-
-    buf.advance(6);
-    println!("{:?}", buf); // "world"
-    println!("{:?}", buf.len());
-    println!("====================");
-
-    let mut buf_mut = BytesMut::from("hello world");
-    println!("{:?}", buf_mut); // "hello world"
-    println!("{:?}", buf_mut.len());
-    println!("{:?}", buf_mut.capacity());
-    println!("====================");
-    buf_mut.reserve(10);
-    println!("{:?}", buf_mut); // "hello world"
-    println!("{:?}", buf_mut.len());
-    println!("{:?}", buf_mut.capacity());
-    println!("====================");
-    unsafe {
-        buf_mut.advance_mut(6);
-    }
-    println!("{:?}", buf_mut); // 剩余的 BytesMut 仍然是 "world"，但前面的数据可能被覆盖
-    println!("{:?}", buf_mut.len());
-    println!("{:?}", buf_mut.capacity());
-    buf_mut.advance(6);
-    println!("{:?}", buf_mut);
-    println!("{:?}", buf_mut.len());
-    println!("{:?}", buf_mut.capacity());
+    let mut v = vec![1, 2, 3, 4, 5];
+    let slice = &v[..];
+    println!("v: {:?}", v);
+    println!("slice: {:?}", slice);
+    v.pop();
+    println!("v: {:?}", v);
+    // println!("slice: {:?}", slice);
+    v[0] = 10;
+    println!("v: {:?}", v);
+    // println!("slice: {:?}", slice);
 }
