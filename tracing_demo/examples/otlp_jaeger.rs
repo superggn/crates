8198ddb75e49e65690e287e423c26043e0844382
superggn <939401399@qq.com>
2025-03-27 19:43:03 +0800

    temp

--- tracing_demo/examples/otlp_jaeger.rs
+++ tracing_demo/examples/otlp_jaeger.rs
@@ -7,9 +7,7 @@ use opentelemetry_sdk::Resource;
 use opentelemetry_sdk::trace::SdkTracerProvider;
 use opentelemetry_stdout as stdout;
 use tracing::{Level, debug, error, info, instrument, span, warn};
-use tracing_subscriber::layer::SubscriberExt;
-use tracing_subscriber::util::SubscriberInitExt;
-use tracing_subscriber::{Registry, registry};
+use tracing_subscriber::{layer::SubscriberExt, registry};
 /// otel/jaeger side
 /// trace => need SpanExporter
 /// protocol (tonic grpc) + port => SpanExporter
@@ -19,6 +17,9 @@ use tracing_subscriber::{Registry, registry};
 /// tracing crate => need Layer
 /// otel tracer + joint crate => tracing layer
 /// layer + Registry => Subscriber
+///
+/// 这里的 tokio::main 应该是 with_tonic 要求的环境，
+/// 如果直接用 http + grpc 的话应该就用不上这个 tokio 了
 #[tokio::main]
 async fn main() {
     // otel side
@@ -26,7 +27,6 @@ async fn main() {
     let otlp_exporter = opentelemetry_otlp::SpanExporter::builder()
         .with_tonic()
         .with_endpoint("http://localhost:4317")
-        // .
         .build()
         .unwrap();
     // std exporter
@@ -36,9 +36,18 @@ async fn main() {
         .with_attributes([KeyValue::new("service.name", "my-awesome-service")])
         .build();
     let provider = SdkTracerProvider::builder()
-        .with_simple_exporter(std_exporter)
-        .with_simple_exporter(otlp_exporter)
+        // simple exporter => 会出现 "命名代码里打了很多 event, 但只传了1个" 这种情况
+        //                 => 这时 jaeger 里就只能看到一条记录， 非常捉急
+        //                 => 正常代码里应该用 BatchSpanProcessor 来着
+        // .with_simple_exporter(std_exporter)
+        // .with_simple_exporter(otlp_exporter)
         .with_resource(resource)
+        .with_span_processor(
+            opentelemetry_sdk::trace::BatchSpanProcessor::builder(std_exporter).build(),
+        )
+        .with_span_processor(
+            opentelemetry_sdk::trace::BatchSpanProcessor::builder(otlp_exporter).build(),
+        )
         .build();
     // Tracer
     let tracer = provider.tracer("readme_example");
@@ -50,39 +59,33 @@ async fn main() {
     // Registry + bridge layer => tracing subscriber
     // let subscriber = Registry::default().with(telemetry).init();
     let subscriber = registry().with(telemetry);
-    // subscriber.init();
-    // subscriber.try_init().unwrap();
-    // println!("do i need init?");
-    // subscriber.try_init();
 
     // Trace executed code
     tracing::subscriber::with_default(subscriber, || {
         // Spans will be sent to the configured OpenTelemetry exporter
-        let root_span = span!(tracing::Level::TRACE, "app_start_hahaha", work_units = 2);
+        info!("お前はもう， 死んでいる");
+        let root_span = span!(Level::TRACE, "app_start_hahaha", work_units = 2);
+        info!("お前はもう， 死んでいる");
         let _enter = root_span.enter();
+        let sub_span = span!(Level::TRACE, "哦豁， 啊哈！");
+        info!("お前はもう， 死んでいる");
+        let _enter_sub = sub_span.enter();
         error!("wuhu!! This event will be logged in the root span.");
         debug!("outer space debug, kid");
         info!("お前はもう， 死んでいる");
-        // my_function(10);
+        my_function_outer(10);
     });
-    // tracing::subscriber::set_global_default(subscriber).unwrap();
-    println!("123");
-    // let sub_span = span!(Level::DEBUG, "my_sub_span", param_temp = 10, jojo = "1st");
-    // info!("这个日志在 span 内部");
-    // let _enter_2 = sub_span.enter();
-    // info!("哈哈哈哈");
-    // drop(_enter_2);
-    // drop(sub_span);
-    my_function(10);
-    // subscriber;
-    provider.force_flush().unwrap();
-    println!("0");
-    provider.shutdown().unwrap();
-    println!("1");
 }
 
 #[instrument] // 自动创建一个 span
-fn my_function(a: u32) {
+fn my_function_outer(a: u32) {
     let x = 42;
+    debug!(x, "计算完成");
+    my_function_inner(2 * a);
+}
+
+#[instrument] // 自动创建一个 span
+fn my_function_inner(a: u32) {
+    let x = 9527;
     debug!(x, "计算完成");
 }
